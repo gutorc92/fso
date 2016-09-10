80700985d92741c1fa9ccbada117ae23d8262d03
pwener <phwener@gmail.com>
2016-09-10 19:07:32 -0300

    Corrigindo q1 do t2

--- trabalho2/q1/alarme.c
+++ trabalho2/q1/alarme.c
@@ -18,8 +18,62 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+void abort_program(void);
+void handle_sigalrm(int signum);
+
+int main(void) {
+  pid_t pid;
+
+  /**
+   * Quando um sinal é lançado ele irá procurar um handler para executar antes
+   * de retornar a execução normalmente. Se essa função:
+   *
+   * void handle_sigalrm(int signum)
+   *
+   * não for criada, ele não irá prosseguir a execução normal.
+   */
+  signal(SIGALRM, handle_sigalrm);
+
+  // Dispara o fork
+  pid = fork();
+
+  if(pid < 0) {
+    printf("Algum erro aconteceu no fork\n");
+    exit(0);
+  }
+
+  /**
+   * Condicao referente ao processo filho, se pid for um numero maior
+   * que 0 está sendo executado dentro do processo pai.
+   */
+  if(pid == 0 ) {
+    printf("Processo filho vai dormir e lançar um sinal. pid = %d\n", getpid());
+
+    int pid_of_father = getpid()-1;
+
+    // Dorme por cinco segundos
+    sleep(5);
+
+    // lanca um sinal que deveria ser capturado com pause
+    kill(pid_of_father, SIGALRM);
+  } else {
+    printf("Processo pai executando e esperando. pid = %d\n", getpid());
+
+    // espera o SIGALRM e imprime sua captura
+    pause();
+
+    printf("Sinal capturado\n");
+  }
+
+  abort_program();
+
+  return 0;
+}
+
 
 void abort_program(void){
   int finish, read_success;
@@ -31,37 +85,14 @@ void abort_program(void){
   }
 }
 
-int main(void) {
-  pid_t pid;
-
-  // Dispara o fork
-  pid = fork();
-
-  if(pid < 0) {
-    printf("Algum erro aconteceu no fork\n");
-    exit(0);
+/**
+ * É necessário um handler para que o sinal seja lançado e o controle seja
+ * retornado para o programa principal(nosso processo pai).
+ */
+void handle_sigalrm(int signum) {
+  if(signum != SIGALRM) {
+    printf("Wrong signal %d", signum);
   }
 
-  // Condicao executada dentro processo filho, se pid for outro numero maior
-  // que 0 está sendo executado dentro do processo pai. Essa é a condição do else
-  if(pid == 0 ) {
-    printf("Processo filho vai dormir e lançar um sinal. pid = %d\n", getpid());
-
-    // Dorme por cinco segundos
-    sleep(5);
-
-    // lanca um sinal que deveria ser capturado com pause
-    alarm(10);
-    printf("Sinal lançado!\n");
-  } else {
-    printf("Processo pai executando e esperando. pid = %d\n", getpid());
-    // espera o SIGALRM
-    pause();
-    printf("Sinal capturado!!!\n");
-    exit(0);
-  }
-
-  abort_program();
-
-  return 0;
+  printf("Lançou um SIGALRM\n");
 }
