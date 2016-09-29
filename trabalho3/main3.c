2c18e42d10a07baf489315f3ca8d8f437e459bcd
Gustavo Coelho <gutorc@hotmail.com>
2016-09-29 02:17:02 -0300

    Usando lib dlfcn para carregar funcoes
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ trabalho3/main3.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <math.h>
+
+int (*gera_primo)();
+
+int main() {
+	void* lib = dlopen("./libprimo.so", RTLD_LAZY);
+	gera_primo = dlsym(lib,"gera_primo");
+
+	for(int count = 0; count < 5; count++) {
+		int prime = gera_primo();
+		printf("Founded prime>> %d\n", prime);
+		sleep(1);
+	}
+	return 0;
+}
